4daebeda25e34992ce01309581dbadb6227c95f7
CarlosDur4n <carloncho3123@gmail.com>
2024-10-21 10:46:54 -0300

    Trabajo grupal TEST

---
+++ app/src/main/java/com/example/actividadgrupal/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.actividadgrupal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView txt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
